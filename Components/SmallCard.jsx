5f4d8001c976b8231097b82f3befe4ec6dcec8f6
vishal <vishaltyagi.dev098@gmail.com>
2023-06-13 16:20:18 +0530

    small card done

--- Components/SmallCard.jsx
+++ Components/SmallCard.jsx
@@ -2,7 +2,17 @@ import React from 'react'
 
 const SmallCard = () => {
   return (
-    <div>SmallCard</div>
+    <div className='shadow-[0_3px_10px_rgb(0,0,0,0.2)] w-[25%] rounded px-4 py-4 bg-white hover:bg-[#a258fe] group'>
+      <div className="my-2 text-[#666767] group-hover:text-white">
+        Female . NL
+      </div>
+      <div className="my-2 text-3xl font-bold group-hover:text-white">
+        Ms. Teatske Nijenhuis
+      </div>
+      <div className="text-[#e16259] group-hover:text-white">
+        gmail.com
+      </div>
+    </div>
   )
 }
 
--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import Navbar from '../Components/Navbar'
+import SmallCard from '../Components/SmallCard'
 
 const index = () => {
   return (
     <div>
       <Navbar/>
+      <SmallCard/>
     </div>
   )
 }
